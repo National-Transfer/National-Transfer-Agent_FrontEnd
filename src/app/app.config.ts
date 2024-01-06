120ca0a2361b4fc1b2c2f66e1aec58a6e7cbd051
Yahya Boujrah <yahyaboujrah@Yahyas-MacBook-Air.local>
2024-01-06 21:07:22 +0100

    add security + octa config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { authInterceptor } from './auth/auth.interceptor';
+import OktaAuth from '@okta/okta-auth-js';
+import { OktaAuthModule } from '@okta/okta-angular';
+import { environment } from '../environments/environment.development';
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-12637298.okta.com/oauth2/default',
+  clientId: '0oae6q7wvhoY28oYV5d7',
+  redirectUri: environment.redirectUri + '/login/callback',
+  responseType: ['token', 'id_token'],
+  pkce: true,
+});
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),provideHttpClient(withInterceptors([authInterceptor])),provideAnimations(),
+  provideHttpClient(withFetch()), provideHttpClient(), importProvidersFrom(OktaAuthModule.forRoot({ oktaAuth })),]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login/callback', component: OktaCallbackComponent , canActivate: [authGuard]},
+    {
+        path: '',
+        loadChildren: () => import('./components/routes').then((m) => m.APP_ROUTES),canActivate: [OktaAuthGuard]
+    },
+];
